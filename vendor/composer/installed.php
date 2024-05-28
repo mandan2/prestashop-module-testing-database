a860a721cb6a8504ef2b2e63d5f71d3f97404754
mandan2 <mantasda2@gmail.com>
2024-05-28 19:10:08 +0300

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mandan2/prestashop-module-testing-database',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '39dd3f90c32bf889b793b05faed187a939ae09cc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mandan2/prestashop-module-testing-database' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '39dd3f90c32bf889b793b05faed187a939ae09cc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
